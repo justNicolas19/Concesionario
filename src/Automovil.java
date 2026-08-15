86f1fd083067394ca548594d125ebdabe72a4ba4
nicolasf <nicolasestebanflorez1357@gmail.com>
2026-08-15 16:52:17 -0500

    Clase Automovil

---
+++ src/Automovil.java
@@ -0,0 +1,31 @@
+public class Automovil extends Vehiculo {
+    private int numeroPuertas;
+    private String tipoCombustible;
+
+    public Automovil(String placa, String marca, String modelo, int anio, double precioBase, int numeroPuertas, String tipoCombustible) {
+        super(placa, marca, modelo, anio, precioBase);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public String getTipoCombustible() {
+        return tipoCombustible;
+    }
+
+    public void setTipoCombustible(String tipoCombustible) {
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        return 0;
+    }
+}
